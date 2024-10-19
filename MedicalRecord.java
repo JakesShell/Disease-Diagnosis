3cc16c1a306e9e3570a19882346c7d55aa9cbd91
Jaco van Rooyen <jacovanrooyen21@gmail.com>
2024-10-19 16:21:36 +0700

    Create  MedicalRecord.java

---
+++ MedicalRecord.java
@@ -0,0 +1,16 @@
+package com.healthcare;
+
+public class MedicalRecord {
+    private double feature1;
+    private double feature2;
+    // Add more features as necessary
+    private String diagnosis;
+
+    public MedicalRecord(double feature1, double feature2, String diagnosis) {
+        this.feature1 = feature1;
+        this.feature2 = feature2;
+        this.diagnosis = diagnosis;
+    }
+
+    // Getters and setters
+}
